822dc332499b6578551825ed87129335806645b1
noushabasiddiqui16-commits <noushabasiddiqui16@gmail.com>
2025-11-09 21:44:13 +0530

    Create Exp_6.c

---
+++ Exp_6.c
@@ -0,0 +1,22 @@
+// Name :- Noushaba Siddiqui
+// Class : Computer Enginnering
+// Division :- D
+// UIN :- 251P180
+
+#include <stdio.h>
+
+void Counter()
+{
+    static int count = 0;
+    count++;
+    printf("Count = %d\n", count);
+}
+
+int main()
+{
+    Counter();
+    Counter();
+    Counter();
+
+    return 0;
+}
